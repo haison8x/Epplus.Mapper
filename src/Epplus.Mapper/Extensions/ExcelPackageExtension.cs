393b8e8a3b76872cecbfe4c36a8982965dcf173d
Jacob.Phan <jacob.phan@nexcel.vn>
2017-06-01 11:25:01 +0700

    GetAsByteArraySafe(string password) - Accept null password

---
+++ src/Epplus.Mapper/Extensions/ExcelPackageExtension.cs
@@ -0,0 +1,20 @@
+﻿using OfficeOpenXml;
+
+namespace Epplus.Mapper.Extensions
+{
+    public static class ExcelPackageExtension
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="excelPackage"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static byte[] GetAsByteArraySafe(this ExcelPackage excelPackage, string password)
+        {
+            return string.IsNullOrEmpty(password)
+                ? excelPackage.GetAsByteArray()
+                : excelPackage.GetAsByteArray(password);
+        }
+    }
+}
---
+++ tests/Epplus.Mapper.UnitTests/Extensions/ExcelPackageExtensionUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using Epplus.Mapper.Extensions;
+using NUnit.CompareNetObjects;
+using NUnit.Framework;
+using OfficeOpenXml;
+
+namespace Epplus.Mapper.UnitTests.Extensions
+{
+    public class ExcelPackageExtensionUnitTests
+    {
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void GetAsByteArraySafe_EmptyPassword_ContentIsNotEncrypted(string password)
+        {
+            // Arrange
+            var expectedExcelPackage = CreateTestExcelPackage();
+            var expected = expectedExcelPackage.GetAsByteArray();
+
+            // Act
+            var excelPackage = CreateTestExcelPackage();
+            var bytes = excelPackage.GetAsByteArraySafe(password);
+
+            // Assert
+            Assert.AreEqual(bytes.Length, expected.Length);
+        }
+
+        [Test]
+        [TestCase("Any Not Empty Password")]
+        public void GetAsByteArraySafe_NonEmptyPassword_ContentIsEncrypted(string password)
+        {
+            // Arrange
+            var expectedExcelPackage = CreateTestExcelPackage();
+            var expected = expectedExcelPackage.GetAsByteArray(password);
+
+            // Act
+            var excelPackage = CreateTestExcelPackage();
+            var bytes = excelPackage.GetAsByteArraySafe(password);
+
+            // Assert
+            Assert.AreEqual(bytes.Length, expected.Length);
+        }
+
+        private ExcelPackage CreateTestExcelPackage()
+        {
+            var excelPackage = new ExcelPackage();
+            var sheet = excelPackage.Workbook.Worksheets.Add("Content");
+            sheet.Cells["A1"].Value = 0;
+
+            return excelPackage;
+        }
+    }
+}
